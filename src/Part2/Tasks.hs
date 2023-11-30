50d1f4c8a6188f1e839810572aa1719c7e63eeb4
vfeofilaktov <vfeofilaktov@ptsecurity.com>
2023-12-01 00:18:15 +0300

    Task 2

--- src/Part2/Tasks.hs
+++ src/Part2/Tasks.hs
@@ -12,18 +12,33 @@ data Term = IntConstant { intValue :: Int }          -- числовая кон�
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) = notImplementedYet
+(|+|) = BinaryTerm Plus
+infixl 7 |+|
 (|-|) :: Term -> Term -> Term
-(|-|) = notImplementedYet
+(|-|) = BinaryTerm Minus
+infixl 7 |-|
 (|*|) :: Term -> Term -> Term
-(|*|) = notImplementedYet
+(|*|) = BinaryTerm Times
+infixl 8 |*|
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = notImplementedYet
+replaceVar varName replacement expression = case expression of
+                                                IntConstant _ -> expression
+                                                BinaryTerm op lhv rhv ->
+                                                   BinaryTerm op (replaceVar varName replacement lhv) (replaceVar varName replacement rhv)
+                                                Variable name -> if name == varName then replacement else expression
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate = notImplementedYet
+evaluate expression = case expression of
+   IntConstant _ -> expression
+   Variable _ -> expression
+   BinaryTerm op lhv rhv -> case (evaluate lhv, evaluate rhv) of
+      (IntConstant i1, IntConstant i2) -> case op of 
+         Plus -> IntConstant (i1 + i2)
+         Minus -> IntConstant (i1 - i2)
+         Times -> IntConstant (i1 * i2)
+      (t1, t2) -> BinaryTerm op t1 t2 
