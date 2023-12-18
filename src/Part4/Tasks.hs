d63dd286e0204e21b0179f1ca53b164f6968e8ae
vfeofilaktov <vfeofilaktov@ptsecurity.com>
2023-12-19 00:45:28 +0300

    Task 4

--- src/Part4/Tasks.hs
+++ src/Part4/Tasks.hs
@@ -1,6 +1,7 @@
 module Part4.Tasks where
 
 import Util(notImplementedYet)
+import Control.Applicative (liftA2)
 
 -- Перевёрнутый связный список -- хранит ссылку не на последующию, а на предыдущую ячейку
 data ReverseList a = REmpty | (ReverseList a) :< a
@@ -16,17 +17,40 @@ rlistToList lst =
 
 -- Реализуйте обратное преобразование
 listToRlist :: [a] -> ReverseList a
-listToRlist = notImplementedYet
+listToRlist = foldr (\ head -> (<>) (REmpty :< head)) REmpty
 
 -- Реализуйте все представленные ниже классы (см. тесты)
-instance Show (ReverseList a) where
-    showsPrec = notImplementedYet
-    show = notImplementedYet
-instance Eq (ReverseList a) where
-    (==) = notImplementedYet
-    (/=) = notImplementedYet
+instance Show a => Show (ReverseList a) where
+    show lst = "[" <> show_ lst <> "]" where
+        show_ REmpty = ""
+        show_ (REmpty :< head) = show head
+        show_ (tail :< head) = show_ tail <> "," <> show head
+
+instance Eq a => Eq (ReverseList a) where
+    (==) REmpty REmpty = True
+    (==) REmpty _ = False
+    (==) _ REmpty = False
+    (==) (tail1 :< head1) (tail2 :< head2) = head1 == head2 && tail1 == tail2
+
+    (/=) l1 l2 = not (l1 == l2)
+
 instance Semigroup (ReverseList a) where
+    (<>) fst REmpty = fst
+    (<>) fst (h :< t) = (fst <> h) :< t
+
 instance Monoid (ReverseList a) where
+    mempty = REmpty
+
 instance Functor ReverseList where
+    fmap f REmpty = REmpty
+    fmap f (h :< t) = fmap f h :< f t
+
 instance Applicative ReverseList where
+    pure a = REmpty :< a
+    liftA2 _ REmpty _ = REmpty
+    liftA2 _ _ REmpty = REmpty
+    liftA2 f (h :< t) x  = liftA2 f h x <> fmap (f t) x
+
 instance Monad ReverseList where
+  (>>=) REmpty f = REmpty
+  (>>=) (h :< t) f = (h >>= f) <> f t
