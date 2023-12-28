3aa0d841362e40a739a34e2835b88942a4439744
vfeofilaktov <vfeofilaktov@ptsecurity.com>
2023-12-28 22:33:32 +0300

    Task 6.0

--- src/Part6/Tasks.hs
+++ src/Part6/Tasks.hs
@@ -3,6 +3,10 @@ module Part6.Tasks where
 
 import Util (notImplementedYet)
 import Data.Map
+import qualified Data.Tree as Prelude
+import qualified Data.Aeson.KeyMap as Map
+import Debug.Trace (trace)
+
 
 -- Разреженное представление матрицы. Все элементы, которых нет в sparseMatrixElements, считаются нулями
 data SparseMatrix a = SparseMatrix {
@@ -14,25 +18,80 @@ data SparseMatrix a = SparseMatrix {
 -- Определите класс типов "Матрица" с необходимыми (как вам кажется) операциями,
 -- которые нужны, чтобы реализовать функции, представленные ниже
 class Matrix mx where
+       -- Реализуйте следующие функции
+       -- Единичная матрица
+       eye :: Int -> mx
+
+       -- Матрица, заполненная нулями
+       zero :: Int -> Int -> mx
+
+       -- Перемножение матриц
+       multiplyMatrix :: mx -> mx -> mx
+
+       -- Определитель матрицы
+       determinant ::  mx -> Int
+
+       element :: mx -> Int -> Int -> Int
+       column :: mx -> Int -> [Int]
+       row :: mx -> Int -> [Int]
+
+       columns :: mx -> Int
+       rows :: mx -> Int
+
 
 -- Определите экземпляры данного класса для:
 --  * числа (считается матрицей 1x1)
 --  * списка списков чисел
 --  * типа SparseMatrix, представленного выше
 instance Matrix Int where
-instance Matrix [[Int]] where
-instance Matrix (SparseMatrix Int) where
+       eye _ = 1
+       zero _ _ = 0
 
--- Реализуйте следующие функции
--- Единичная матрица
-eye :: Matrix m => Int -> m
-eye w = notImplementedYet
--- Матрица, заполненная нулями
-zero :: Matrix m => Int -> Int -> m
-zero w h = notImplementedYet
--- Перемножение матриц
-multiplyMatrix :: Matrix m => m -> m -> m
-multiplyMatrix = notImplementedYet
--- Определитель матрицы
-determinant :: Matrix m => m -> Int
-determinant = notImplementedYet
+       multiplyMatrix m1 m2 = m1 * m2
+       determinant m = m
+
+       element mx _ _ = mx
+       column mx _ = [mx]
+       row mx _ = [mx]
+
+       columns _ = 1
+       rows _ = 1
+
+instance Matrix [[Int]] where
+       eye i = Prelude.map (\x -> (replicate x 0 ++ [1]) ++ replicate (i - x - 1) 0) [0..(i-1)]
+       zero h w = replicate w (replicate h 0)
+
+       element mx y x = mx !! x !! y
+       column mx i = Prelude.map (!! i) mx
+       row mx i = mx !! i
+
+       multiplyMatrix m1 m2 = Prelude.map (\m1row -> Prelude.map (\m2col -> dot (row m1 m1row) (column m2 m2col)) [0..(columns m2 - 1)]) [0..(rows m1 - 1)]
+       determinant m = notImplementedYet
+
+       columns mx = length (head mx)
+       rows = length
+
+instance Matrix (SparseMatrix Int) where
+       eye i = SparseMatrix w h (Data.Map.fromList d) where
+              w = i
+              h = i
+              d = Prelude.map (\x -> ((x, x), 1)) [0..(i-1)]
+
+       zero h w = SparseMatrix w h Data.Map.empty
+
+       element mx x y = if member (x, y) (sparseMatrixElements mx) then sparseMatrixElements mx ! (x, y) else 0
+       column mx i = Prelude.map (element mx i) [0..(sparseMatrixHeight mx)]
+       row mx i = Prelude.map (\x -> element mx x i) [0..(sparseMatrixWidth mx)]
+
+       multiplyMatrix m1 m2 = SparseMatrix w h (Data.Map.fromList (Prelude.concat d)) where
+              w = sparseMatrixWidth m2
+              h = sparseMatrixHeight m1
+              d = Prelude.map (\m1row -> Prelude.map (\m2col -> ((m2col, m1row), dot (row m1 m1row) (column m2 m2col))) [0..(columns m2 - 1)]) [0..(rows m1 - 1)]
+       determinant m = notImplementedYet
+       columns = sparseMatrixWidth
+       rows = sparseMatrixHeight
+
+dot :: [Int] -> [Int] -> Int
+dot v1 v2 = sum (zipWith (*) v1 v2)
+
+sMatrixToMatrix mx = Prelude.map (\column -> Prelude.map (element mx column) [0..(rows mx - 1)]) [0..(columns mx - 1)]
--- src/Part6/Tests.hs
+++ src/Part6/Tests.hs
@@ -22,3 +22,55 @@ unit_zero = do
     zero 5 5 @?= SparseMatrix 5 5 (Data.Map.fromList ([]::[((Int, Int), Int)]))
     where zz :: Int; zz = 0
 
+unit_mult_scalars = do
+    multiplyMatrix m1 m2 @?= 1 * 2
+    multiplyMatrix m2 m3 @?= 2 * 10
+    where
+        m1 :: Int;
+        m2 :: Int;
+        m3 :: Int
+        m1 = 1;
+        m2 = 2;
+        m3 = 10;
+
+unit_mult_matrix = do
+    multiplyMatrix m1 m2 @?= [[1 * 1 + 2 * 2 + 3 * 3]]
+    multiplyMatrix m1 m3 @?= [[1 * 1 + 2 * 4 + 3 * 7, 1 * 2 + 2 * 5 + 3 * 8, 1 * 3 + 2 * 6 + 3 * 9]]
+    multiplyMatrix m3 m2 @?= [[1 * 1 + 2 * 2 + 3 * 3], [4 * 1 + 5 * 2 + 6 * 3], [7 * 1 + 8 * 2 + 9 * 3]]
+    where
+        m1 :: [[Int]];
+        m2 :: [[Int]];
+        m3 :: [[Int]]
+        m1 = [[1, 2, 3]];
+        m2 = [[1], [2], [3]];
+        m3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+unit_sMatrixToMatrix = do
+    sMatrixToMatrix m1 @?= l1
+    sMatrixToMatrix m2 @?= l2
+    sMatrixToMatrix m3 @?= l3
+
+    where
+        m1 :: SparseMatrix Int;
+        m2 :: SparseMatrix Int;
+        m3 :: SparseMatrix Int;
+
+        m1 = SparseMatrix 2 2 (Data.Map.fromList [((0, 0), 1), ((1, 1), 10)])
+        m2 = SparseMatrix 3 3 (Data.Map.fromList [((0, 0), 1), ((0, 1), 2), ((0, 2), 3), ((1, 0), 4), ((1, 1), 5), ((1, 2), 6), ((2, 0), 7), ((2, 1), 8), ((2, 2), 9)])
+        m3 = SparseMatrix 1 3 (Data.Map.fromList [((0, 0), 1), ((0, 1), 2), ((0, 2), 3)])
+        l1 = [[1, 0], [0, 10]]
+        l2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        l3 = [[1, 2, 3]]
+
+unit_mult_s_matrix = do
+    sMatrixToMatrix (multiplyMatrix m1 m2) @?= [[3], [3], [0]]
+    sMatrixToMatrix (multiplyMatrix m1 m2) @?= [[3], [3], [0]]
+    sMatrixToMatrix (multiplyMatrix m3 m1) @?= [[1,0,1],[0,0,0],[1,0,1]]
+
+    where
+        m1 :: SparseMatrix Int;
+        m2 :: SparseMatrix Int;
+        -- m3 :: SparseMatrix Int
+        m1 = SparseMatrix 3 1 (Data.Map.fromList [((0, 0), 1), ((2, 0), 1)]);
+        m2 = SparseMatrix 3 3 (Data.Map.fromList [((0, 0), 1), ((1, 0), 2), ((1, 1), 3), ((2, 1), 1), ((0, 2), 2), ((1, 2), 1)]);
+        m3 = SparseMatrix 1 3 (Data.Map.fromList [((0, 0), 1), ((0, 2), 1)]);
